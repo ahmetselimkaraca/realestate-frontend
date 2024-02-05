39e1f56c363a91dc702b81e23f090dd0dc150f3d
ahmetselimkaraca <ahmetselimkaraca@gmail.com>
2024-02-05 22:25:07 +0300

    add axios interceptors

--- src/App.jsx
+++ src/App.jsx
@@ -10,27 +10,27 @@ import MapPage from "./pages/MapPage";
 import Admin from "./pages/Admin";
 import axios from "axios";
 
-// axios.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response.status === 401 && window.location.pathname !== "/") {
-//       localStorage.removeItem("token");
-//       window.location.href = "/";
-//     }
-//     return Promise.reject(error);
-//   }
-// );
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status === 401 && window.location.pathname !== "/") {
+      localStorage.removeItem("token");
+      window.location.href = "/";
+    }
+    return Promise.reject(error);
+  }
+);
 
-// axios.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("token");
-//     if (token) {
-//       config.headers["Authorization"] = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 function App() {
   return (
